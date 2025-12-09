f3907d4920bb950f5aa5f3cca127eff360cbfa6b
pmnoos <petermagner3@gmail.com>
2025-12-09 13:16:54 +1000

    Updated purchases 09/12/2025

---
+++ food_costs/globals.d.ts
@@ -0,0 +1,7 @@
+// Ensure VS Code recognises the Bootstrap JS global
+declare var bootstrap: any;
+
+// Ensure browser DOM globals are available
+declare var document: Document;
+declare var window: Window;
+
